414b755a81b8cc48117ce466d710c8c19efb9ddc
Harish Veeramani <hdv987@gmail.com>
2018-01-09 23:59:29 -0600

    Add config

---
+++ app/src/main/java/com/harish/solstice/solsticecontacts/util/Config.java
@@ -0,0 +1,13 @@
+package com.harish.solstice.solsticecontacts.util;
+
+/**
+ * Created by Harish Veeramani on 1/9/2018.
+ */
+
+public class Config {
+    public final static String CONTACT_KEY = "CONTACT_KEY";
+    public final static int INTENT_CODE = 1000;
+
+    public final static String API_MAIN_URL = "https://s3.amazonaws.com";
+    public final static String API_CALL = "/technical-challenge/v3/contacts.json";
+}
