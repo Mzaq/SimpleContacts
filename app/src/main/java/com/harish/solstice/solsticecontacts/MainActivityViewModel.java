fb6271b148f6af40dc43440daa967c1438ae0ecd
Harish Veeramani <hdv987@gmail.com>
2018-01-10 00:00:15 -0600

    Add view model and adapter

---
+++ app/src/main/java/com/harish/solstice/solsticecontacts/MainActivityViewModel.java
@@ -0,0 +1,86 @@
+package com.harish.solstice.solsticecontacts;
+
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+import com.harish.solstice.solsticecontacts.api.APIService;
+import com.harish.solstice.solsticecontacts.obj.Contact;
+import com.harish.solstice.solsticecontacts.util.Config;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by Harish Veeramani on 1/9/2018.
+ */
+
+public class MainActivityViewModel extends ViewModel {
+    private Retrofit mRetrofit;
+    private CompositeDisposable mCompositeDisposable;
+    private MutableLiveData<Boolean> mShowHideLoadingImage;
+    private MutableLiveData<Boolean> mShowErrorMessage;
+    private MutableLiveData<List<Contact>> mShowPhoneBook;
+
+    public MainActivityViewModel() {
+        super();
+
+        mRetrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(Config.API_MAIN_URL)
+                .build();
+
+        mCompositeDisposable = new CompositeDisposable();
+        mShowErrorMessage = new MutableLiveData<>();
+        mShowHideLoadingImage = new MutableLiveData<>();
+        mShowPhoneBook = new MutableLiveData<>();
+    }
+
+    public void callPhoneBookService(){
+        APIService service = mRetrofit.create(APIService.class);
+
+        mCompositeDisposable.add(service.getCompletePhonebook()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse, this::handleError));
+
+        setShowHideLoadingImage(true);
+    }
+
+    private void handleResponse(List<Contact> contactList) {
+        setShowPhoneBook(contactList);
+        setShowHideLoadingImage(false);
+    }
+
+    private void handleError(Throwable error) {
+        mShowErrorMessage.setValue(true);
+        setShowHideLoadingImage(false);
+    }
+
+    public MutableLiveData<Boolean> getShowHideLoadingImage() {
+        return mShowHideLoadingImage;
+    }
+
+    public void setShowHideLoadingImage(Boolean showHideStatus) {
+        this.mShowHideLoadingImage.setValue(showHideStatus);
+    }
+
+    public MutableLiveData<Boolean> getShowErrorMessage() {
+        return mShowErrorMessage;
+    }
+
+    public MutableLiveData<List<Contact>> getShowPhoneBook() {
+        return mShowPhoneBook;
+    }
+
+    private void setShowPhoneBook(List<Contact> contactList) {
+        this.mShowPhoneBook.setValue(contactList);
+    }
+}
+
---
+++ app/src/main/java/com/harish/solstice/solsticecontacts/MainRecyclerViewAdapter.java
@@ -0,0 +1,153 @@
+package com.harish.solstice.solsticecontacts;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.harish.solstice.solsticecontacts.obj.Contact;
+import com.harish.solstice.solsticecontacts.util.CustomEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Harish Veeramani on 1/9/2018.
+ */
+
+public class MainRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+    private LayoutInflater mLayoutInflater;
+    private List<Object> mItems;
+    private Context mContext;
+    private CustomEventListener mCustomEventListener;
+
+    private static final int CONTACT_VIEW_TITLE = 0;
+    private static final int CONTACT_VIEW_ITEM = 1;
+
+    public MainRecyclerViewAdapter(Context context, LayoutInflater layoutInflater, CustomEventListener eventListener) {
+        this.mLayoutInflater = layoutInflater;
+        this.mContext = context;
+        this.mCustomEventListener = eventListener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case CONTACT_VIEW_TITLE:
+                View viewTitle = mLayoutInflater.inflate(R.layout.list_title, parent, false);
+                return new TitleViewHolder(viewTitle);
+            case CONTACT_VIEW_ITEM:
+                View viewItem = mLayoutInflater.inflate(R.layout.list_item_contact, parent, false);
+                return new ContactViewHolder(viewItem, mContext, mCustomEventListener);
+        }
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Object item = mItems.get(position);
+
+        if (holder instanceof TitleViewHolder) {
+            ((TitleViewHolder)holder).bindTitle(item.toString());
+        }
+
+        if (holder instanceof ContactViewHolder) {
+            ((ContactViewHolder)holder).bindContact((Contact)item);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mItems != null){
+            return mItems.size();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mItems.get(position) instanceof String){
+            return CONTACT_VIEW_TITLE;
+        } else {
+            return CONTACT_VIEW_ITEM;
+        }
+    }
+
+    public void addContactList(List<Object> items) {
+        if (this.mItems != null && !this.mItems.isEmpty()) {
+            this.mItems.clear();
+        }
+        this.mItems = items;
+        notifyDataSetChanged();
+    }
+
+    public class ContactViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.contactImage)
+        ImageView contactImage;
+        @BindView(R.id.contactName)
+        TextView contactName;
+        @BindView(R.id.starImage)
+        ImageView favoriteImage;
+        @BindView(R.id.descriptionText)
+        TextView descriptionText;
+
+        private Context context;
+        private Contact mContact;
+        private CustomEventListener mCustomEventListener;
+
+        public ContactViewHolder(View itemView, Context context, CustomEventListener eventListener) {
+            super(itemView);
+            this.context = context;
+            this.mCustomEventListener = eventListener;
+            itemView.setOnClickListener(this);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindContact(Contact contact) {
+            this.mContact = contact;
+
+            contactName.setText(contact.getName());
+
+            Picasso.with(context)
+                    .load(contact.getSmallImageURL())
+                    .placeholder(R.drawable.ic_placeholder_image)
+                    .into(contactImage);
+
+            if (contact.getIsFavorite()){
+                favoriteImage.setVisibility(View.VISIBLE);
+            } else {
+                favoriteImage.setVisibility(View.INVISIBLE);
+            }
+
+            descriptionText.setText(contact.getCompanyName());
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mCustomEventListener != null) {
+                mCustomEventListener.onItemClicked(mContact);
+            }
+        }
+    }
+
+    public class TitleViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.title_text)
+        TextView mTitleText;
+
+        public TitleViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bindTitle(String title) {
+            mTitleText.setText(title);
+        }
+    }
+}
